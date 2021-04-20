98b3bebef9862a25b3ea3d08c1aef8ffdfcbb338
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-04-21 00:21:32 +0200

    Add Mute

---
+++ Assets/Scripts/MuteController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteController : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnMouseDown()
+    {
+        GetComponent<Text>().text = audioSource.mute ? "Mute" : "Unmute";
+        audioSource.mute = !audioSource.mute;
+    }
+}
