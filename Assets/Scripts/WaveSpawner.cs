5e0ec50cec61df8fc7e731ec97a2219c453082e9
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-06-04 13:26:38 +0200

    Rework waves

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -4,23 +4,16 @@ using UnityEngine;
 public class WaveSpawner : MonoBehaviour
 {
     public GameObject spawner;
-    public GameObject archer;
-    public GameObject swordsman;
-    public GameObject guard;
-    public GameObject wizard;
+    public List<GameObject> units;
 
     public bool spawning;
 
-    private Queue<GameObject> unitQueue = new Queue<GameObject>();
+    private readonly Queue<GameObject> unitQueue = new Queue<GameObject>();
 
     void Start()
     {
         InvokeRepeating("SpawnWave", 2f, 0.1f);
-        InvokeRepeating("Group1", 0f, 20f);
-        InvokeRepeating("Group2", 30f, 14f);
-        InvokeRepeating("Group3", 60f, 24f);
-        InvokeRepeating("Group4", 120f, 20f);
-        InvokeRepeating("Group5", 240f, 26f);
+        InvokeRepeating("AddUnit", 0f, 16f);
     }
 
     private void SpawnWave()
@@ -34,41 +27,8 @@ public class WaveSpawner : MonoBehaviour
             unitQueue.Dequeue();
     }
 
-    private void Group1()
+    private void AddUnit()
     {
-        unitQueue.Enqueue(swordsman);
-        unitQueue.Enqueue(swordsman);
-    }
-
-    private void Group2()
-    {
-        CancelInvoke("Group1");
-        unitQueue.Enqueue(swordsman);
-        unitQueue.Enqueue(archer);
-    }
-
-    private void Group3()
-    {
-        CancelInvoke("Group2");
-        unitQueue.Enqueue(guard);
-        unitQueue.Enqueue(guard);
-        unitQueue.Enqueue(guard);
-    }
-
-    private void Group4()
-    { 
-        CancelInvoke("Group3");
-        unitQueue.Enqueue(guard);
-        unitQueue.Enqueue(wizard);
-        unitQueue.Enqueue(archer);
-    }
-
-    private void Group5()
-    {
-        CancelInvoke("Group4");
-        unitQueue.Enqueue(guard);
-        unitQueue.Enqueue(archer);
-        unitQueue.Enqueue(wizard);
-        unitQueue.Enqueue(swordsman);
+        unitQueue.Enqueue(units[Random.Range(0, units.Count)]);
     }
 }
